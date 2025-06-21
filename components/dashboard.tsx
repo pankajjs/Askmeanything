e7e4702b572c699478aed9f09a46a28688209b1c
Pankaj <pankajshadev@gmail.com>
2025-06-22 02:18:53 +0530

    feat: add question component shimmer

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { CardDescription, CardFooter, CardHeader } from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
 
 import {useCallback, useContext, useEffect, useState } from "react";
 import { Card } from "./ui/card";
@@ -84,7 +85,11 @@ const Questions = ({date, answered}: {date: Date, answered: string}) => {
   }, [fetchQuestions, answered])
 
   if(isLoading){
-    return <div className="text-center text-sm text-muted-foreground">Loading...</div>
+    return <>
+      <QuestionSkeleton />
+      <QuestionSkeleton />
+      <QuestionSkeleton />
+    </>
   }
 
   if(questions.length == 0){
@@ -122,4 +127,24 @@ const Questions = ({date, answered}: {date: Date, answered: string}) => {
       </CardFooter>
     </Card>
   ))
+}
+
+const QuestionSkeleton = () => {
+  return (
+    <Card className="w-full mx-auto gap-0 my-2 p-0 shadow-md border rounded-md">
+      <CardHeader className="flex flex-row justify-end items-center gap-2 px-4 pt-2 pb-0">
+        <Skeleton className="h-4 w-4" />
+        <Skeleton className="h-4 w-24" />
+      </CardHeader>
+      <CardDescription
+        className="px-4 py-2 pb-1 flex justify-center items-center min-h-20 border-b-1 text-sm font-medium w-full break-words break-all whitespace-pre-wrap overflow-x-auto"
+      >
+        <Skeleton className="h-10 w-full" />
+      </CardDescription>
+      <CardFooter className="flex justify-between px-4 py-2">
+        <Skeleton className="h-8 w-8" />
+        <Skeleton className="h-8 w-8" />
+      </CardFooter>
+    </Card>
+  )
 }
