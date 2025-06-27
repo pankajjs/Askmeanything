8ed00c226ad3fc7c297afd0c695e6f1bd7db31be
Pankaj <pankajshadev@gmail.com>
2025-06-27 23:24:36 +0530

    fix: refactor api caller

--- lib/api/questions.ts
+++ lib/api/questions.ts
@@ -9,7 +9,7 @@ export const createQuestion = async ({data, username, createdBy}: {data: string,
     
     const jsonRes = await res.json();
 
-    if(!res.ok){
+    if(!jsonRes.success){
       throw new Error(jsonRes.message)
     }
   
@@ -24,7 +24,7 @@ export const deleteQuestion = async(id: string) => {
     
     const jsonRes = await res.json();
 
-    if(!res.ok){
+    if(!jsonRes.success){
       throw new Error(jsonRes.message)
     }
   
