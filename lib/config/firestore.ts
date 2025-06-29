cf27ee52d7dffba36a7b16687ec995b13702b924
Pankaj <pankajshadev@gmail.com>
2025-06-29 22:42:27 +0530

    fix: refactor firestore initialization

--- lib/config/firestore.ts
+++ lib/config/firestore.ts
@@ -1,11 +1,16 @@
-import { initializeApp, cert, ServiceAccount, getApps } from 'firebase-admin/app';
+import 'server-only'
+
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getFirestore }  from 'firebase-admin/firestore';
-import serviceAccount from "./prod.firestore.json";
 
 // Check if Firebase app is already initialized
 if (getApps().length === 0) {
   initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
+    credential: cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
+    })
   });
 }
 
