2484b6c43e3cfc6bcb8234f0bcec1f919f0ece62
Pankaj <pankajshadev@gmail.com>
2025-07-03 00:53:18 +0530

    chore: import module for dev env with condition

--- lib/config/firestore.ts
+++ lib/config/firestore.ts
@@ -2,13 +2,12 @@ import 'server-only'
 
 import { initializeApp, cert, getApps, ServiceAccount } from 'firebase-admin/app';
 import { getFirestore }  from 'firebase-admin/firestore';
-import serviceAccount from "./prod.firestore.json";
 
 const firestoreConfig = process.env.NODE_ENV === "production" ? {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
-}: serviceAccount;
+}: import("./prod.firestore.json");
 
 // Check if Firebase app is already initialized
 if (getApps().length === 0) {
