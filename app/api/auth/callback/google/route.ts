6fe65931e7c874d81d6c5e32192b0f2222d216cf
Pankaj <pankajshadev@gmail.com>
2025-07-02 21:35:08 +0530

    fix: refactor auth callback and replace with correct ttl

--- app/api/auth/callback/google/route.ts
+++ app/api/auth/callback/google/route.ts
@@ -4,32 +4,32 @@ import { generateToken } from "@/lib/jwt";
 import { cookies } from "next/headers";
 import { config } from "@/lib/config/config";
 import { createUser, findUserByEmail } from "@/lib/dao/users";
+import { UnauthorizedError } from "@/lib/errors";
 
 export async function GET(req: NextRequest) {
     try{
         const state = req.nextUrl.searchParams.get("state")
+        
         if(!state){
-            console.error("Failed to get state from google auth");
-            return NextResponse.redirect(new URL("/", req.url))
+            throw new UnauthorizedError("State not found");
         }
 
         const code = req.nextUrl.searchParams.get("code")
+        
         if(!code){
-            console.error("Failed to get code from google auth");
-            return NextResponse.redirect(new URL("/", req.url))
+            throw new UnauthorizedError("Code not found");
         }
         
         const token = await oauth2Client.getToken(code)
+        
         if(!token || !token.tokens.access_token){
-            console.error("Failed to get token from google auth");
-            return NextResponse.redirect(new URL("/", req.url))
+            throw new UnauthorizedError("Token not found");
         }
 
         const tokenInfo = await oauth2Client.getTokenInfo(token.tokens.access_token);
 
         if(!tokenInfo.email_verified || !tokenInfo.email){
-            console.error("Email not verified");
-            return NextResponse.redirect(new URL("/", req.url))
+            throw new UnauthorizedError("Email not verified");
         }
         
         let user = await findUserByEmail(tokenInfo.email);
@@ -47,12 +47,12 @@ export async function GET(req: NextRequest) {
         cookieStore.set(config.userToken.cookieName, accessToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            maxAge: parseInt(process.env.USER_TOKEN_TTL!)
+            maxAge: config.userToken.ttl
         })
 
         return NextResponse.redirect(state)
     }catch(error){
         console.error("Error while logging in", error)
-        return NextResponse.redirect(new URL("/", req.url))
+        return NextResponse.redirect(new URL(`/?error=unauthorized`, req.url))
     }
 }
